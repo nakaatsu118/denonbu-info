cb83822446d2f7dffbfb8de0fc128ae77e937956
Atsushi Nakashima <nakaatsumain@gmail.com>
2022-12-15 22:19:29 +0900

    トップページを最新楽曲のみに変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,28 +4,30 @@ import { client } from '~/utils/microCMSClient';
 import styles from '../styles/Home.module.scss';
 import { parseISO, format } from 'date-fns';
 import Meta from '~/components/_common/Meta';
+import Footer from '~/components/Footer';
+import Link from 'next/link';
 
 export interface Props {
   data: Song[];
+  totalCount: number;
 }
 
 const isLazy = (index: Number) => {
   return index >= 8 ? 'lazy' : 'eager';
 }
 
-const Home = ({ data }: Props): JSX.Element => {
+const Home = ({ data, totalCount }: Props): JSX.Element => {
   return (
     <div className={styles.page}>
       <div className={styles.header}>
-        <a href='./' rel="noopener noreferrer" aria-label='Top'>
-          <img className={`${styles.headerImage} ${styles.blur}`} src='image/logo_trans.png' alt='logo' width={128} height={88} />
-        </a>
+        <Link href='/' passHref>
+          <img className={`${styles.headerImage} ${styles.blur}`} src='/image/logo_trans.png' alt='logo' width={128} height={88} />
+        </Link>
       </div>
       <div className={styles.container}>
         <Meta pageTitle="" pageUrl="" pageOgImage="" />
         <div className={styles.topContainer} >
-          <h1>MUSICS</h1>
-          <h2>電音部の楽曲情報、配信リンクをまとめています</h2>
+          <h1>NEW MUSIC</h1>
         </div>
         <div className={styles.main}>
           {data.map((song, index) => (
@@ -78,38 +80,10 @@ const Home = ({ data }: Props): JSX.Element => {
             </div>
           ))}
         </div>
-        <div className={styles.footer}>
-          <div className={styles.desc}>
-            <h3>このサイトについて</h3>
-            <p>
-              電音部の楽曲情報（YouTube, Twitter
-              etc…）が混在していて配信サイト一覧などが追いづらい状態だったので、一覧で見やすいサイトが欲しくなったので作成しました。
-            </p>
-            <h3>TODO</h3>
-            <ul>
-              <li>フィルター機能（エリア、キャラクター）</li>
-              <li>並び替え（リリース日昇順降順）</li>
-              <li>ページング</li>
-            </ul>
-            <h3>ポリシー</h3>
-            <ul className={styles.policy}>
-              <li>当サイトは電音部の非公式ファンサイトです。</li>
-              <li>バンダイナムコエンターテインメント様とは一切関係ありません。</li>
-              <li>当サイトで使用している画像の著作権は各権利者に帰属いたします。</li>
-              <li>ご意見ご要望ございましたら下記Twitterアカウントまでご連絡ください。</li>
-
-              <li>
-                このコンテンツはファンメイドコンテンツです。ファンメイドコンテンツポリシー（
-                <a href="https://denonbu.jp/guidelines">https://denonbu.jp/guidelines</a>）のもと制作されています。
-              </li>
-            </ul>
-          </div>
-          <div className={styles.copyright}>
-            <a href="https://twitter.com/nakaatsu" target="_blank" rel="noopener noreferrer">
-              @nakaatsu
-            </a>
-          </div>
+        <div className={styles.musicButton}>
+          <Link href='/music/1' passHref><p>全楽曲一覧</p></Link>
         </div>
+        <Footer />
       </div>
     </div>
   );
@@ -118,11 +92,12 @@ const Home = ({ data }: Props): JSX.Element => {
 export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await client.get({ endpoint: 'music', queries: { limit: 100 } });
+  const data = await client.get({ endpoint: 'music', queries: { offset: 0, limit: 4 } });
 
   return {
     props: {
       data: data.contents,
+      totalCount: data.totalCount
     },
   };
 };
